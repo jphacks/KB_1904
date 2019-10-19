2d1a0056e168a26a423ad5361a040e0e34fd9830
john <johnbardera@gmail.com>
2019-10-20 04:09:59 +0900

    fix auth.ts

--- front/src/api/auth.ts
+++ front/src/api/auth.ts
@@ -1,13 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
 import { HttpClient } from '@angular/common/http';
 import { Child, Parent } from 'src/models';
+import { RemoveToken, SetToken } from '../store/jwt-token.store';
+import { AppState } from '../store';
+
+export interface TokenParam {
+  token: string;
+}
 
 @Injectable()
-export class Auth {
-  constructor(private http: HttpClient) {}
+export class AuthApi {
+  key = 'jwt-token';
+  constructor(private store: Store<AppState>, private http: HttpClient) {}
 
-  // login(parent: Parent, child: Child, password: string) {
-  //   this.http.post<>('login/')
-  // }
-  register() {}
+  register(_parent: Parent, _child: Child, _password: string): Observable<string> {
+    return this.http
+      .post('register', {
+        parent: _parent,
+        child: _child,
+        password: _password,
+      })
+      .pipe(
+        map((_: TokenParam) => {
+          this.setToken(_.token);
+          return _.token;
+        })
+      );
+  }
+
+  login(_email: string, _password: string): Observable<string> {
+    return this.http
+      .post('login', {
+        email: _email,
+        password: _password,
+      })
+      .pipe(
+        map((_: TokenParam) => {
+          this.setToken(_.token);
+          return _.token;
+        })
+      );
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.key);
+  }
+
+  existToken(): boolean {
+    const token = localStorage.getItem(this.key);
+    return token === null;
+  }
+
+  private setToken(token: string) {
+    this.store.dispatch(new SetToken(token));
+  }
+
+  removeToken() {
+    this.store.dispatch(new RemoveToken());
+  }
 }
