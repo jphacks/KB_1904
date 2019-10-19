e4797752343bafb63e5448380d2ed550ba462e9e
Kazuya Izumi <shikibu9419@gmail.com>
2019-10-19 16:14:53 +0900

    Add seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,35 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-unless Parent.count > 0
+
+unless Parent.count.positive? && Child.count.positive?
   parent = Parent.create!(name: 'お母さん')
-
   child = Child.create!(parent: parent, name: 'ほげの', sex: :female)
+end
 
-  multiline_desc = <<-EOF
-  庭に突如発生した謎の怪物.
-  我等の計画を邪魔するものは直ちに排除せよ.
-  EOF
-
+unless Quest.count.positive?
   Quest.create!(
-    [
-      {
-        parent: parent,
-        child: child,
-        title: '宿題をしよう!',
-        description: 'しような',
-        point: 50
-      },
-      {
-        parent: parent,
-        child: child,
-        title: 'やばいやつを倒して！',
-        description: multiline_desc,
-        point: 1000
-      }
-    ]
+    parent: parent,
+    child: child,
+    title: '宿題をしよう!',
+    description: 'しような',
+    point: 50
   )
 
+  multiline_desc = <<-EOF
+庭に突如発生した謎の怪物.
+我等の計画を邪魔するものは直ちに排除せよ.
+EOF
+
+  quest = Quest.create!(
+    parent: parent,
+    child: child,
+    title: 'やばいやつを倒して！',
+    description: multiline_desc,
+    quest_type: :weekly,
+    point: 1000
+  )
+
+  quest.day_of_weeks.create!(
+    [
+      { quest: quest, code: 0 },
+      { quest: quest, code: 2 },
+      { quest: quest, code: 4 }
+    ]
+  )
+end
+
+unless Reward.count.positive?
   Reward.create!(
     [
       {
