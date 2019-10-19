b17dfdfb5b28a9c937c20b85eed3be127252ab1d
john <johnbardera@gmail.com>
2019-10-20 03:56:01 +0900

    やったーーーー！！！！！！！！ログイン通りました！！！！

--- server/config/application.rb
+++ server/config/application.rb
@@ -11,6 +11,20 @@ module Server
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # config.action_dispatch.default_headers = {
+    # 'Access-Control-Allow-Credentials' => 'true',
+    # 'Access-Control-Allow-Origin' => 'http://localhost:8100',
+    # 'Access-Control-Request-Method' => '*'
+# }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '*path' => 'options_request#preflight', via: :options
+
   devise_for :parents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
