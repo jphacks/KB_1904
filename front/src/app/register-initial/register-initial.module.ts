b4e77401d9afe7d82a9a6302bf324daa5e9041d9
根津宏輔 <kousuke@ip-10-117-30-150.ap-northeast-1.compute.internal>
2019-10-20 04:25:17 +0900

    レジスター諸々

--- front/src/app/register-initial/register-initial.module.ts
+++ front/src/app/register-initial/register-initial.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -10,17 +10,12 @@ import { RegisterInitialPage } from './register-initial.page';
 const routes: Routes = [
   {
     path: '',
-    component: RegisterInitialPage
-  }
+    component: RegisterInitialPage,
+  },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [RegisterInitialPage]
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  declarations: [RegisterInitialPage],
 })
 export class RegisterInitialPageModule {}
