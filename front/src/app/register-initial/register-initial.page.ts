292d1c0e7cac598604fcfe2ca04c8ca0594ee0cb
根津宏輔 <kousuke@ip-10-117-30-150.ap-northeast-1.compute.internal>
2019-10-20 04:25:22 +0900

    見た目

--- front/src/app/register-initial/register-initial.page.ts
+++ front/src/app/register-initial/register-initial.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Parent, Child } from '../../models';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register-initial',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-initial.page.scss'],
 })
 export class RegisterInitialPage implements OnInit {
+  formNumber = 0;
+  form1 = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+  form2 = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    sex: new FormControl('', [Validators.required]),
+  });
+  form3 = new FormGroup({
+    whosePhone: new FormControl('', [Validators.required]),
+  });
+  parent: Parent;
+  child: Child;
+  password: string;
 
-  constructor() { }
+  constructor(private router: Router, private toastCtrl: ToastController) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async register1(form: FormGroup, event) {
+    if (form.invalid) {
+      const toast = await this.toastCtrl.create({ message: 'フォームの値が無効です', duration: 1500 });
+      toast.present();
+      return;
+    }
+    this.parent.name = form.value.name;
+    this.parent.email = form.value.email;
+    this.password = form.value.password;
+    this.formNumber = 1;
   }
-
+  async register2(form: FormGroup, event) {
+    if (form.invalid) {
+      const toast = await this.toastCtrl.create({ message: 'フォームの値が無効です', duration: 1500 });
+      toast.present();
+      return;
+    }
+    this.child.name = form.value.name;
+    this.child.sex = form.value.sex;
+    this.formNumber = 2;
+  }
+  register3(isParent: boolean) {}
 }
