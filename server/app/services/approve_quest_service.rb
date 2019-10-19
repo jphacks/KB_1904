0708855caadbf1109424b681067d0192f3fa52b6
Kazuya Izumi <shikibu9419@gmail.com>
2019-10-19 14:55:32 +0900

    Implement AddPointService

--- server/app/services/approve_quest_service.rb
+++ server/app/services/approve_quest_service.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
 class ApproveQuestService
-  attr_accessor :quest, :params
+  attr_accessor :quest, :parent, :child
 
   def initialize(quest)
     @quest = quest
+    @parent = quest.parent
+    @child = quest.child
   end
 
   def execute!
     return false unless quest.status.finished?
 
+    AddPointService.new(child).execute!(quest.point)
     quest.update!(status: :approved)
 
     QuestAchievement.create!(
-      parent: quest.parent,
-      child: quest.child,
+      parent: parent,
+      child: child,
       quest_id: quest.id,
       title: quest.title,
       point: quest.point
