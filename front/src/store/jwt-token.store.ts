5b49a470ee6494ba104bc264d168c1ba03d8057f
john <johnbardera@gmail.com>
2019-10-20 00:10:20 +0900

    add jwt-token

---
+++ front/src/store/jwt-token.store.ts
@@ -0,0 +1,38 @@
+import { Action } from '@ngrx/store';
+
+export const SET_TOKEN = '[Jwt Token] SET_TOKEN';
+export const REMOVE_TOKEN = '[Jwt Token] REMOVE_TOKEN';
+
+export class SetToken implements Action {
+  readonly type = SET_TOKEN;
+  constructor(public token: string) {}
+}
+export class RemoveToken implements Action {
+  readonly type = REMOVE_TOKEN;
+}
+
+export type All = SetToken | RemoveToken;
+
+export interface State {
+  token: string;
+}
+export const initialState = {
+  token: localStorage.getItem('jwt-token'),
+};
+
+export function reducer(state: State = initialState, action: All): State {
+  switch (action.type) {
+    case SET_TOKEN: {
+      const token = action.token;
+      localStorage.setItem('jwt-token', token);
+      return { token };
+    }
+    case REMOVE_TOKEN: {
+      localStorage.removeItem('jwt-token');
+      return { token: null };
+    }
+    default: {
+      return state;
+    }
+  }
+}
