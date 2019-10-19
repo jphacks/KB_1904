a38a732bd9bfb26b94a41c13e6a50d287e9718af
Kazuya Izumi <shikibu9419@gmail.com>
2019-10-20 07:06:44 +0900

    Add quest api but not (ry

--- front/src/app/quest/quest-detail/quest-detail.page.ts
+++ front/src/app/quest/quest-detail/quest-detail.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../store';
+import { Store, select } from '@ngrx/store';
+import { QuestService } from '../../../service';
+import { Quest } from '../../../models';
+import { selectQuest } from '../../../store/quest.store';
 
 @Component({
   selector: 'app-quest-detail',
@@ -6,7 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quest-detail.page.scss'],
 })
 export class QuestDetailPage implements OnInit {
-  constructor() {}
+  questId: number;
+  quest$: Observable<Quest>;
 
-  ngOnInit() {}
+  constructor(
+    private questSvc: QuestService,
+    public navParams: NavParams,
+    private store: Store<AppState>,
+  ) {
+    this.questId = this.navParams.get('questId');
+    this.quest$ = this.store.pipe(
+      select(selectQuest(this.questId))
+    );
+  }
+
+  ngOnInit() {
+    this.questSvc.get(this.questId).subscribe();
+  }
+
+  approve() {
+    this.questSvc.approve(this.questId).subscribe();
+  }
+
+  reject() {
+    const quest = { id: this.questId, status: 'none' };
+    this.questSvc.update(quest).subscribe();
+  }
 }
