12ad5ef5f2a519aa4d81ac46ccbce2083ed96cf3
根津宏輔 <kousuke@nezusuke.local>
2019-10-20 14:13:54 +0900

    赤いの消した

--- front/src/app/quest/quest-detail/quest-detail.page.ts
+++ front/src/app/quest/quest-detail/quest-detail.page.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { AppState } from '../../../store';
 import { Store, select } from '@ngrx/store';
 import { QuestService } from '../../../service';
 import { Quest } from '../../../models';
 import { selectQuest } from '../../../store/quest.store';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quest-detail',
@@ -16,15 +16,9 @@ export class QuestDetailPage implements OnInit {
   questId: number;
   quest$: Observable<Quest>;
 
-  constructor(
-    private questSvc: QuestService,
-    public navParams: NavParams,
-    private store: Store<AppState>,
-  ) {
-    this.questId = this.navParams.get('questId');
-    this.quest$ = this.store.pipe(
-      select(selectQuest(this.questId))
-    );
+  constructor(private questSvc: QuestService, private store: Store<AppState>, private route: ActivatedRoute) {
+    this.questId = Number(this.route.snapshot.paramMap.get('id'));
+    this.quest$ = this.store.pipe(select(selectQuest(this.questId)));
   }
 
   ngOnInit() {
