8c978f4f9ea7db9760fc0ddc30b7ae630d494099
根津宏輔 <kousuke@ip-192-168-100-58.ap-northeast-1.compute.internal>
2019-10-20 09:24:43 +0900

    クエスト編集画面追加

---
+++ front/src/app/quest-edit/quest-edit.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quest-edit',
+  templateUrl: './quest-edit.page.html',
+  styleUrls: ['./quest-edit.page.scss'],
+})
+export class QuestEditPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
