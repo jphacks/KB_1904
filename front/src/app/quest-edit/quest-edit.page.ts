9049f3924caf7dc7b18470d3c48f23378e334558
根津宏輔 <kousuke@ip-192-168-100-58.ap-northeast-1.compute.internal>
2019-10-20 09:49:40 +0900

    追記

--- front/src/app/quest-edit/quest-edit.page.ts
+++ front/src/app/quest-edit/quest-edit.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quest-edit',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quest-edit.page.scss'],
 })
 export class QuestEditPage implements OnInit {
-
-  constructor() { }
+  form = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    questType: new FormControl('', [Validators.required]),
+    point: new FormControl('', [Validators.required]),
+    datetime: new FormControl('', []),
+    description: new FormControl('', []),
+  });
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // ここら辺でidのあるなしを見て新規作成か更新か変えようと思ってる
   }
-
 }
